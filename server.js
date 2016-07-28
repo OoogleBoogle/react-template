9bc4bc03c067235559babf9b3432cf49b94b0a33
OoogleBoogle <simattfield@gmail.com>
2016-07-28 14:50:38 -0700

    updated server file

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
-require('babel-register');
-require('babel-polyfill');
-require('./express/server');
+var express = require('express'),
+    bodyParser = require('body-parser'),
+    path = require('path');
+
+var app = express();
+let indexPage = path.join(__dirname, 'dist/index.html');
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
+app.get('/*', function() {
+    res.sendFile(indexPage);
+});
+
+app.set('port', process.env.NODE_PORT || 3000);
+
+app.listen(app.get('port'),function() {
+  console.log("Listeing on Port " + app.get('port'));
+});
